3fa3abd7f4c077a1f944a3366c8340f478486ca3
AmritaSingh0202 <147167787+AmritaSingh0202@users.noreply.github.com>
2024-07-19 13:50:08 +0530

    Final Student Profile Sect

--- client/src/Student/YourProfile/YourProfile.js
+++ client/src/Student/YourProfile/YourProfile.js
@@ -1,74 +1,64 @@
 import React, { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import "./YourProfile.css";
-
 import axios from "axios";
 import Navbar from "../../components/Navbar/Navbar";
 
 const YourProfile = () => {
-  const loginUser = "student";
-
   const [profile, setProfile] = useState({
-    studentFullName: "Anuruddh Singh",
-    studentEmail: "anuruddh7234@gmail.com",
-    studentMobileNo: "8795734013",
-    studentGender: "Male",
-    studentAge: "20",
-    studentAddress: "Lucknow, Uttar Pradesh",
-    studentProfilePicture: "path-to-profile-picture.jpg",
-
+    studentFullName: "",
+    studentUserName: "",
+    studentEmail: "",
+    studentGender: "",
+    studentAge: "",
+    studentProfilePicture: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
     setProfile((prevProfile) => ({
       ...prevProfile,
       [name]: value,
     }));
   };
 
-  const handleSubmit = async () => {
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const body = {
       studentFullName: profile.studentFullName,
+      studentUserName: profile.studentUserName,
       studentEmail: profile.studentEmail,
-      studentMobileNo: profile.studentMobileNo,
       studentGender: profile.studentGender,
       studentAge: profile.studentAge,
-      studentAddress: profile.studentAddress,
       studentProfilePicture: profile.studentProfilePicture,
-    }
+    };
 
     const config = {
-
       headers: {
-        "content": "multipart/form-data",
-  
+        "Content-Type": "application/json",
       },
+      withCredentials: true,
+    };
 
-      withCredentials: true,   /// this is for reading the cookie from the server side
-    }
-
-    const response = await axios.post(`api/student/profile/${loginUser}`, body, config);
-
-    console.log("response =>", response);
-
-
-  };
-
-  const fetchProfile = async () => {
     try {
-      const response = await axios.get(`api/student/profile/${loginUser}`);
-      console.log("get all courses response=>", response);
-      console.log("response.data =>", response.data);
-      console.log("response.data.data =>", response.data.data);
-      setProfile(response.data.data);
+      const response = await axios.put(`/api/student/update`, body, config);
+      console.log("response =>", response);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const fetchProfile = async () => {
+    try {
+      const response = await axios.get(`/api/student/getProfile`, {
+        withCredentials: true,
+      });
+      const data = response.data;
+      setProfile(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleAvatarChange = (e) => {
     const file = e.target.files[0];
@@ -77,7 +67,7 @@ const YourProfile = () => {
       reader.onloadend = () => {
         setProfile((prevProfile) => ({
           ...prevProfile,
-          profilePicture: reader.result,
+          studentProfilePicture: reader.result,
         }));
       };
       reader.readAsDataURL(file);
@@ -86,7 +76,6 @@ const YourProfile = () => {
 
   useEffect(() => {
     fetchProfile();
-
   }, []);
 
   return (
@@ -98,12 +87,11 @@ const YourProfile = () => {
             <div className="profile-header">
               <div className="avatar-container">
                 <Avatar
-                  src={profile.profilePicture}
+                  src={profile.studentProfilePicture}
                   alt="Profile"
                   className="avatar"
                   onClick={() => document.getElementById("avatarInput").click()}
                 />
-
                 <input
                   type="file"
                   id="avatarInput"
@@ -114,47 +102,42 @@ const YourProfile = () => {
               </div>
               <h2>My Profile</h2>
             </div>
-
-    
             <form className="profile-form" onSubmit={handleSubmit}>
               <div className="form-row">
                 <div className="form-group">
                   <label>Full Name *</label>
                   <input
                     type="text"
-                    name="fullName"
+                    name="studentFullName"
                     value={profile.studentFullName}
                     onChange={handleChange}
                   />
                 </div>
-
+                <div className="form-group">
+                  <label>Username *</label>
+                  <input
+                    type="text"
+                    name="studentUserName"
+                    value={profile.studentUserName}
+                    onChange={handleChange}
+                  />
+                </div>
                 <div className="form-group">
                   <label>Email *</label>
                   <input
                     type="email"
-                    name="email"
-                    value={profile.email}
+                    name="studentEmail"
+                    value={profile.studentEmail}
                     onChange={handleChange}
                   />
                 </div>
               </div>
-
               <div className="form-row">
-                <div className="form-group">
-                  <label>Mobile No *</label>
-                  <input
-                    type="tel"
-                    name="mobileNo"
-                    value={profile.mobileNo}
-                    disabled
-                  />
-                </div>
-
                 <div className="form-group">
                   <label>Gender</label>
                   <select
-                    name="gender"
-                    value={profile.gender}
+                    name="studentGender"
+                    value={profile.studentGender}
                     onChange={handleChange}
                   >
                     <option value="Male">Male</option>
@@ -162,33 +145,19 @@ const YourProfile = () => {
                     <option value="Other">Other</option>
                   </select>
                 </div>
-              </div>
-
-              <div className="form-row">
                 <div className="form-group">
                   <label>Age</label>
                   <input
                     type="number"
-                    name="age"
-                    value={profile.age}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Address</label>
-                  <input
-                    type="text"
-                    name="address"
-                    value={profile.address}
+                    name="studentAge"
+                    value={profile.studentAge}
                     onChange={handleChange}
                   />
                 </div>
               </div>
-
               <button type="submit" className="save-changes">
                 Save changes
               </button>
-
             </form>
           </div>
         </main>
